85015cd7ed38ad8e8d31eba070915109cc664c8b
WMCB-Tech <bautista.wyatt.marcus30@gmail.com>
2021-07-05 15:11:14 +0800

    add a script to install bob the fish

---
+++ user-home/install_btfish.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# do not run this as root if you want to set fish as non-root shell
+
+set -e -u
+# -e: exits when there's an error
+# -u: aborts if there's any unset variables
+
+realuser="$(whoami)"
+
+# check root
+if [ -f "/system/bin/app_process" ]; then
+	SUDO=""
+else
+	if [ "$(id)" == "0" ]; then
+		SUDO=""
+	else
+		SUDO="sudo -s"
+	fi
+if
+
+# setup fish if haven't installed yet
+if ! "$(command -v fish)" >/dev/null 2>&1; then
+	# install fish
+	echo "Installing fish shell..."
+	${SUDO} bash -c "apt install fish git || pacman -S fish git || dnf install fish git || apk add fish git"
+
+	echo "Setting up fish shell as your default shell"
+	# change shell as fish
+	${SUDO} chsh -s "$(command -v fish)" "${realuser}"
+fi
+
+# install bob the fish
+if [ ! -e "${HOME-:/home/${realuser}}/.local/share/omf/themes/bobthefish" ]; then
+	echo "Installing the fish theme: bob the fish"
+	fish -c "omf install bobthefish"
+	fish -c "omf theme bobthefish"
+else
+	echo "Bob the Fish is already installed! skipping install and setting it"
+	fish -c "omf theme bobthefish"
+fi
+
+# alert a user
+cat <<- EOM
+	Successfully Installed bob-the-fish! and already set as your default theme!
+
+	If it feels like it isn't applied yet, run "omf doctor" to diagnose and give solution
+EOM
