3af21e0056d1d5cebe1991b11429326cd054cf24
WMCB-Tech <bautista.wyatt.marcus30@gmail.com>
2021-07-05 15:14:43 +0800

    some modifications

--- user-home/install_btfish.sh
+++ user-home/install_btfish.sh
@@ -22,7 +22,7 @@ if
 if ! "$(command -v fish)" >/dev/null 2>&1; then
 	# install fish
 	echo "Installing fish shell..."
-	${SUDO} bash -c "apt install fish git || pacman -S fish git || dnf install fish git || apk add fish git"
+	${SUDO} bash -c "apt install fish git curl || pacman -S fish git curl || dnf install fish git curl || apk add fish git curl"
 
 	echo "Setting up fish shell as your default shell"
 	# change shell as fish
@@ -31,6 +31,9 @@ fi
 
 # install bob the fish
 if [ ! -e "${HOME-:/home/${realuser}}/.local/share/omf/themes/bobthefish" ]; then
+	echo "Setting up oh-my-fish for you"
+	curl -L https://get.oh-my.fish | fish
+
 	echo "Installing the fish theme: bob the fish"
 	fish -c "omf install bobthefish"
 	fish -c "omf theme bobthefish"
